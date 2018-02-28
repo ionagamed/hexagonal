e2be1c28503d25946976bc27223b48646d01f650
camillakrr <camilla111@ya.ru>
2018-02-28 15:33:36 +0300

    place search.js content in a closure

--- hexagonal/ui/static/search.js
+++ hexagonal/ui/static/search.js
@@ -1,34 +1,34 @@
-// (function() {
-const setMenuToSearch = function () {
-    $('.item.active').removeClass('active');
-    $('#menu-search-field-item').addClass('active');
-};
+(function () {
+    const setMenuToSearch = function () {
+        $('.item.active').removeClass('active');
+        $('#menu-search-field-item').addClass('active');
+    };
 
-const searchIntro = function (term) {
-    if (!window.searchStateChanged) {
-        window.searchStateChanged = true;
-        if (window.history.pushState) {
-            window.history.pushState("", "Hexagonal", '/admin/search?search=' + term);
+    const searchIntro = function (term) {
+        if (!window.searchStateChanged) {
+            window.searchStateChanged = true;
+            if (window.history.pushState) {
+                window.history.pushState("", "Hexagonal", '/admin/search?search=' + term);
+            }
+            setMenuToSearch();
+        } else {
+            if (window.history.replaceState) {
+                window.history.replaceState("", "Hexagonal", '/admin/search?search=' + term);
+            }
         }
-        setMenuToSearch();
-    } else {
-        if (window.history.replaceState) {
-            window.history.replaceState("", "Hexagonal", '/admin/search?search=' + term);
-        }
-    }
-};
+    };
 
-const search = function (e) {
-    const term = $('#search-field').val();
-    const url = '/admin/search_inplace?search=' + term;
-    searchIntro(term);
-    $.ajax({
-        url: url
-    }).then(function (r) {
-        $('#page-container').html(r);
-    });
-};
+    const search = function (e) {
+        const term = $('#search-field').val();
+        const url = '/admin/search_inplace?search=' + term;
+        searchIntro(term);
+        $.ajax({
+            url: url
+        }).then(function (r) {
+            $('#page-container').html(r);
+        });
+    };
 
-$('#search-field').change(search);
-$('#search-field').keyup(search);
-// })();
+    $('#search-field').change(search);
+    $('#search-field').keyup(search);
+})();
